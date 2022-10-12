aff71e247dfd477225c716c9fab44be09ec7a412
Yunus Emre Altanay <yunus.altanay@iceberg-digital.co.uk>
2022-10-12 17:47:37 +0300

    new migration system added

---
+++ db/migrations/000001_init.down.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS sms_template_categories
+DROP TABLE IF EXISTS sms_template_types
+DROP TABLE IF EXISTS sms_templates
+
---
+++ db/migrations/000001_init.up.sql
@@ -0,0 +1,56 @@
+CREATE TABLE "sms_templates"
+(
+    "sms_template_id"          SERIAL PRIMARY KEY NOT NULL,
+    "company_id"               integer,
+    "branch_id"                integer,
+    "name"                     text,
+    "subject"                  text,
+    "content"                  text,
+    "subscription_type_id"     integer,
+    "sms_template_category_id" integer,
+    "created_by"               integer,
+    "sms_template_type_id"     integer,
+    "activity_id"              integer,
+    "is_edited"                boolean,
+    "created_at"               timestamp DEFAULT 'now()',
+    "updated_at"               timestamp,
+    "deleted_at"               timestamp
+);
+
+CREATE TABLE "sms_template_types"
+(
+    "sms_template_type_id" SERIAL PRIMARY KEY NOT NULL,
+    "name"                 text,
+    "description"          text,
+    "key"                  text,
+    "created_at"           timestamp DEFAULT 'now()',
+    "updated_at"           timestamp,
+    "deleted_at"           timestamp
+);
+
+CREATE TABLE "sms_template_categories"
+(
+    "sms_template_category_id" SERIAL PRIMARY KEY NOT NULL,
+    "company_id"               integer,
+    "branch_id"                integer,
+    "name"                     text,
+    "description"              text,
+    "created_by"               integer,
+    "created_at"               timestamp DEFAULT 'now()',
+    "updated_at"               timestamp,
+    "deleted_at"               timestamp
+);
+
+CREATE INDEX ON "sms_templates" ("sms_template_type_id");
+
+CREATE INDEX ON "sms_templates" ("sms_template_category_id");
+
+CREATE INDEX ON "sms_template_types" ("key");
+
+CREATE INDEX ON "sms_template_categories" ("company_id");
+
+ALTER TABLE "sms_templates"
+    ADD FOREIGN KEY ("subscription_type_id") REFERENCES "sms_template_types" ("sms_template_type_id");
+
+ALTER TABLE "sms_templates"
+    ADD FOREIGN KEY ("sms_template_category_id") REFERENCES "sms_template_categories" ("sms_template_category_id");
