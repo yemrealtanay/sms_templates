c4fa0b58ff35c098e8c465ddd629e240877d4c8e
Yunus Emre Altanay <yunus.altanay@iceberg-digital.co.uk>
2022-10-17 09:44:39 +0300

    query implementation added

--- db/sqlc/sms_template_test.go
+++
@@ -1,7 +0,0 @@
-package db
-
-import "testing"
-
-func TestQueries_CreateSmsTemplate(t *testing.T) {
-	arg := CreateSmsTemplateParams{}
-}
---
+++ db/sqlc/store.go
@@ -0,0 +1,39 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
+}
+
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx.err: %v, rb err: %v", err, rbErr)
+		}
+		return err
+	}
+	return tx.Commit()
+}
+
+func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	
+}
