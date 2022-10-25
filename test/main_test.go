99900fde24d544c3abba43ffc0dd6be374d4abbc
Yunus Emre Altanay <yunus.altanay@iceberg-digital.co.uk>
2022-10-25 13:35:59 +0300

    folder structer adjusted

--- test/main_test.go
+++ test/main_test.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/yemrealtanay/sms_templates/db/sqlc"
 	"github.com/yemrealtanay/sms_templates/util"
 	"log"
@@ -15,7 +16,8 @@ var testQueries *db.Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	config, err := util.LoadConfig("../..")
+	config, err := util.LoadConfig("../config/")
+	fmt.Println(config)
 	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to database:", err)
