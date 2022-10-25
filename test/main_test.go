b42da0f01a6524404dcd6ad95e7a4182adb4ac98
Yunus Emre Altanay <yunus.altanay@iceberg-digital.co.uk>
2022-10-25 13:02:24 +0300

    main test file fixed

--- test/main_test.go
+++ test/main_test.go
@@ -15,7 +15,7 @@ var testQueries *db.Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	config, err := util.LoadConfig("../..")
+	config, err := util.LoadConfig("../config/..")
 	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to database:", err)
